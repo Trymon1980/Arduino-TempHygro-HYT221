aab50353b5c5cd5f74daf4a44c8de88345a39791
Trymon <git@mi-gas.de>
2023-07-15 17:12:01 +0200

    Modified code to utilize HYT221 instead of DHT11. Updated README

--- Arduino-TempHygro-HYT221.ino
+++ Arduino-TempHygro-HYT221.ino
@@ -1,34 +1,63 @@
 //Include libraries.
 #include <Wire.h>                                           //Already installed with Arduino IDE
-#include <DHT.h>                                            //DHT sensor library by Adafruit
 #include <LiquidCrystal_I2C.h>                              //LiquidCrystal I2C by Marco Schwartz
 
 //For ease of use some definitions
-#define DHTPIN 2                                            //Datapin for DHT11 connected to D2
-#define DHTTYPE DHT11                                       //Used DHT type
+#define HYTADDR 0x28                                        //Address for HYT 221 communication
 #define LCDI2CADDRESS 0x27                                  //Address for I2C communication                   
 #define LCDCOLUMS 16                                        //Number of LCD Display colums
 #define LCDLINES 2000                                       //Number of LCD Display lines
 
 LiquidCrystal_I2C lcd(LCDI2CADDRESS, LCDCOLUMS, LCDLINES);  //Create lcd object with defined parameters
-DHT dht(DHTPIN, DHTTYPE, 6);                                //Create dht object with defined parameters. Third parameter not used anymore by library. Default of 6 is a good option.
 
 void setup()
 {
   lcd.init();                                               //Initialize lcd
   lcd.backlight();                                          //Turn on backlight of LCD
-  dht.begin();                                              //Initialize dht
 }
 
 void loop()
 {
-  lcd.clear();                                              //Clear screen of LCD
-  lcd.print("T:  ");                                        //Print in first line of LCD the shown string
-  lcd.print(String(dht.readTemperature(), 2));              //Print Temperature reading of DHT as String
-  lcd.print(" C");                                          //Print C for Celsius at the end
-  lcd.setCursor (0,1);                                      //Move cursor to first colum in second line
-  lcd.print("rF: ");                                        //Print rF for relative Feuchte (relative Humidity)
-  lcd.print(String(dht.readHumidity(), 2));                 //print Humidity reading of DHT as String
-  lcd.print(" %");                                          //print % at the end
-  delay(2000);                                              //Delay loop for 2000ms (2 seconds)
+  double humidity;                                          //Variable for huiḿidity
+  double temperature;                                       //Variable for temperature
+
+  Wire.beginTransmission(HYTADDR);                          // Begin transmission with given device on I2C bus
+  Wire.requestFrom(HYTADDR, 4);                             // Request 4 bytes
+
+  // Read the bytes if they are available
+  // The first two bytes are humidity the last two are temperature
+  if(Wire.available() == 4) 
+  {                   
+    int b1 = Wire.read();
+    int b2 = Wire.read();
+    int b3 = Wire.read();
+    int b4 = Wire.read();
+    
+    Wire.endTransmission();                                 // End transmission and release I2C bus
+
+    int rawHumidity = b1 << 8 | b2;                         // combine humidity bytes 
+    rawHumidity =  (rawHumidity &= 0x3FFF);                 // compound bitwise to get 14 bit measurement first two bits are status/stall bit
+    humidity = 100.0 / pow(2,14) * rawHumidity;             //calculate humidity. Calculation provided in HYT221 documentation
+    
+    // combine temperature bytes and calculate temperature
+    b4 = (b4 >> 2); // Mask away 2 least significant bits see HYT 221 doc
+    int rawTemperature = b3 << 6 | b4;
+    temperature = 165.0 / pow(2,14) * rawTemperature - 40;  //calculate temperature. Calculation provided in HYT221 documentation
+  
+    lcd.clear();                                            //Clear screen of LCD
+    lcd.print("T:  ");                                      //Print in first line of LCD the shown string
+    lcd.print(String(temperature, 7));                      //Print Temperature reading of HYT211 as String with 7 decimal digits. Only 6 are delivered. 7th is always 0.
+    lcd.print(" C");                                        //Print C for Celsius at the end
+    lcd.setCursor (0,1);                                    //Move cursor to first colum in second line
+    lcd.print("rF: ");                                      //Print rF for relative Feuchte (relative Humidity)
+    lcd.print(String(humidity, 7));                         //Print Humidity reading of HYT211 as String with 7 decimal digits. Only 6 are delivered. 7th is always 0.
+    lcd.print(" %");                                        //print % at the end
+    delay(2000);                                            //Delay loop for 2000ms (2 seconds)
+  }
+  else
+  {
+    lcd.clear();
+    lcd.print("Not enough bytes available on wire.");
+    
+  }
 }
--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 ## Used Libraries:
 ```
 Wire.h, included with Arduino IDE
-DHT.h; DHT sensor library by Adafruit; https://www.arduinolibraries.info/libraries/dht-sensor-library
 LiquidCrystal_I2C.h; LiquidCrystal I2C by Marco Schwartz; https://www.arduinolibraries.info/libraries/liquid-crystal-i2-c
 ```
 
@@ -43,6 +42,6 @@ IST HYT 221: https://www.ist-ag.com/sites/default/files/downloads/HYT221.pdf
 
 This project is to utilize an Arduino nano with the IST HYT 221 high precission temperature and humidity 
 sensor for very precise readings. Display out via 16x2 LCD.
-In the current state DH11 is used instead of HYT221 to start the Project.
-As soon HYT221 is delivered the code will be rewritten to utilize the correct sensor.
-README won't cover details of DH11 and this will be removed as soon the chosen sensor could be used. 
+HYT221 reasings up to 6 decimal digits with high precission. 
+Arduino code to receive information adapted from https://github.com/stylesuxx/Arduino-HYT-221-I2C/tree/master
+
